d010848a4f8b28d2c2ea252386e632a353089a59
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-19 00:16:36 +0200

    Make root private.

--- binarytrees/binarytree.go
+++ binarytrees/binarytree.go
@@ -5,16 +5,16 @@ import "github.com/src2crs/algovis/binarytrees/binarytreenode"
 // BinaryTree represents a binary tree.
 // It has a single field, Root, which is a pointer to the root node of the tree.
 type BinaryTree struct {
-	Root *binarytreenode.Node
+	root *binarytreenode.Node
 }
 
 // NewBinaryTree creates a new binary tree with an empty root node.
 func NewBinaryTree() *BinaryTree {
-	return &BinaryTree{Root: binarytreenode.NewRootNode()}
+	return &BinaryTree{root: binarytreenode.NewRootNode()}
 }
 
 // IsEmpty returns true if the tree is empty.
 // A tree is empty if its root node is empty.
 func (t *BinaryTree) IsEmpty() bool {
-	return t.Root.IsEmpty()
+	return t.root.IsEmpty()
 }
