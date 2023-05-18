5c3832c526ea1d082d1fb00262e45698ad477382
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-18 19:41:33 +0200

    Refactor NodeInfo tests.

--- binarytrees/node_nodeinfo_test.go
+++ binarytrees/node_nodeinfo_test.go
@@ -1,20 +1,23 @@
 package binarytrees
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/src2crs/algovis/graphdrawing"
+)
 
 // TestNode_NodeInfo_root_empty tests whether the NodeInfo() method
 // returns the correct NodeInfo for an empty root node without a label.
 func TestNode_NodeInfo_root_empty_nolabel(t *testing.T) {
 	root := NewRootNode()
-	info := root.NodeInfo()
-	if info.Name != "root" {
-		t.Errorf("root.NodeInfo().Name is %s, but expected %s", info.Name, "root")
+	actualinfo := root.NodeInfo()
+	expectedinfo := graphdrawing.NodeInfo{
+		Name:    "root",
+		Label:   "",
+		Visible: false,
 	}
-	if info.Label != "" {
-		t.Errorf("root.NodeInfo().Label is %s, but expected %s", info.Label, "")
-	}
-	if info.Visible {
-		t.Errorf("root.NodeInfo().Visible is %t, but expected %t", info.Visible, false)
+	if actualinfo != expectedinfo {
+		t.Errorf("root.NodeInfo() is %#v, but expected %#v", actualinfo, expectedinfo)
 	}
 }
 
@@ -23,15 +26,14 @@ func TestNode_NodeInfo_root_empty_nolabel(t *testing.T) {
 func TestNode_NodeInfo_root_nonempty_nolabel(t *testing.T) {
 	root := NewRootNode()
 	root.CreateChild("L")
-	info := root.NodeInfo()
-	if info.Name != "root" {
-		t.Errorf("root.NodeInfo().Name is %s, but expected %s", info.Name, "root")
+	actualinfo := root.NodeInfo()
+	expectedinfo := graphdrawing.NodeInfo{
+		Name:    "root",
+		Label:   "",
+		Visible: true,
 	}
-	if info.Label != "" {
-		t.Errorf("root.NodeInfo().Label is %s, but expected %s", info.Label, "")
-	}
-	if !info.Visible {
-		t.Errorf("root.NodeInfo().Visible is %t, but expected %t", info.Visible, true)
+	if actualinfo != expectedinfo {
+		t.Errorf("root.NodeInfo() is %#v, but expected %#v", actualinfo, expectedinfo)
 	}
 }
 
@@ -40,15 +42,14 @@ func TestNode_NodeInfo_root_nonempty_nolabel(t *testing.T) {
 func TestNode_NodeInfo_root_empty_label(t *testing.T) {
 	root := NewRootNode()
 	root.SetLabel("foo")
-	info := root.NodeInfo()
-	if info.Name != "root" {
-		t.Errorf("root.NodeInfo().Name is %s, but expected %s", info.Name, "root")
+	actualinfo := root.NodeInfo()
+	expectedinfo := graphdrawing.NodeInfo{
+		Name:    "root",
+		Label:   "foo",
+		Visible: false,
 	}
-	if info.Label != "foo" {
-		t.Errorf("root.NodeInfo().Label is %s, but expected %s", info.Label, "foo")
-	}
-	if info.Visible {
-		t.Errorf("root.NodeInfo().Visible is %t, but expected %t", info.Visible, false)
+	if actualinfo != expectedinfo {
+		t.Errorf("root.NodeInfo() is %#v, but expected %#v", actualinfo, expectedinfo)
 	}
 }
 
@@ -58,14 +59,13 @@ func TestNode_NodeInfo_root_nonempty_label(t *testing.T) {
 	root := NewRootNode()
 	root.CreateChild("L")
 	root.SetLabel("foo")
-	info := root.NodeInfo()
-	if info.Name != "root" {
-		t.Errorf("root.NodeInfo().Name is %s, but expected %s", info.Name, "root")
+	actualinfo := root.NodeInfo()
+	expectedinfo := graphdrawing.NodeInfo{
+		Name:    "root",
+		Label:   "foo",
+		Visible: true,
 	}
-	if info.Label != "foo" {
-		t.Errorf("root.NodeInfo().Label is %s, but expected %s", info.Label, "foo")
-	}
-	if !info.Visible {
-		t.Errorf("root.NodeInfo().Visible is %t, but expected %t", info.Visible, true)
+	if actualinfo != expectedinfo {
+		t.Errorf("root.NodeInfo() is %#v, but expected %#v", actualinfo, expectedinfo)
 	}
 }
