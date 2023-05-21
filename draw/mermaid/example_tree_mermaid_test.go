2ef48d3782fdc3137476c32134ccbeba290c91eb
Reiner Hüchting <dev@reinerhuechting.de>
2023-05-22 01:32:56 +0200

    Add another mermaid example for a ternary tree.

--- draw/mermaid/example_tree_mermaid_test.go
+++ draw/mermaid/example_tree_mermaid_test.go
@@ -39,3 +39,54 @@ func ExampleMermaid_binarytree_complete() {
 	//   1 --- 1.0
 	//   1 --- 1.1
 }
+
+// ExampleMermaid_ternarytree_complete shows how to create an incomplete binary tree using
+// the tree.Tree data type.
+// It also shows how to draw it using the Mermaid function.
+func ExampleMermaid_ternarytree_complete() {
+	// Create a ternary tree of height 2
+	t := tree.New()
+
+	// Use the AddNodeAtPath function to add all the leaf nodes.
+	// The inner nodes will be added automatically.
+	t.AddNodeAtPath("0.0")
+	t.AddNodeAtPath("0.1")
+	t.AddNodeAtPath("0.2")
+	t.AddNodeAtPath("1.0")
+	t.AddNodeAtPath("1.1")
+	t.AddNodeAtPath("1.2")
+	t.AddNodeAtPath("2.0")
+	t.AddNodeAtPath("2.1")
+	t.AddNodeAtPath("2.2")
+
+	// Draw the tree
+	fmt.Println(Mermaid(t))
+
+	// Output:
+	// graph TD
+	//   root[root]
+	//   0[0]
+	//   0.0[0.0]
+	//   0.1[0.1]
+	//   0.2[0.2]
+	//   1[1]
+	//   1.0[1.0]
+	//   1.1[1.1]
+	//   1.2[1.2]
+	//   2[2]
+	//   2.0[2.0]
+	//   2.1[2.1]
+	//   2.2[2.2]
+	//   root --- 0
+	//   0 --- 0.0
+	//   0 --- 0.1
+	//   0 --- 0.2
+	//   root --- 1
+	//   1 --- 1.0
+	//   1 --- 1.1
+	//   1 --- 1.2
+	//   root --- 2
+	//   2 --- 2.0
+	//   2 --- 2.1
+	//   2 --- 2.2
+}
